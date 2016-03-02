0c51c78f36be32fb8104dd945e953ea6a7f6a296
Yuyue <yuyuez@uw.edu>
2016-03-02 08:52:09 -0800

    a8

--- built_in_scatter.R
+++ built_in_scatter.R
@@ -2,9 +2,9 @@ library(plotly)
 library(dplyr)
  
   plotScatter <- function(irisData, len, wid, marker){
-  
+        
     plot_ly(data = irisData, x = eval(parse(text = len)), y = eval(parse(text = wid)), 
-        mode = "markers", marker = list(size = marker, color = toRGB("blue")) %>%
+            mode = "markers", marker = list(size = marker, color = toRGB("blue")) %>%
   return()
     )
 }
--- ui.R
+++ ui.R
@@ -10,12 +10,6 @@ shinyUI(fluidPage(
   sidebarLayout(
      # Four widgets are used to select species, x and y axises, and to adjust slider 
     sidebarPanel("side panel",
-      
-      # This is a widget called radio button used to choose a species            
-      #radioButtons("Iris", label = h3("Iris Species"), 
-       #         choices = list("Setosa" = "setosa", "versicolor" = "versicolor", 
-        #                      "virginica" = "virginica"), 
-         #       selected = "setosa"),
     
       # This is a widget called select box used to select what's on the x-axis
       selectInput("Xaxis", label = h3("X-axis"), 
@@ -23,13 +17,13 @@ shinyUI(fluidPage(
                               "Petal Width" = "Petal.Width"), 
                 selected = "Petal.Length"),
     
-    # This is a widget called select box used to select what's on the y-axis
-    selectInput("Yaxis", label = h3("Y-axis"), 
+      # This is a widget called select box used to select what's on the y-axis
+      selectInput("Yaxis", label = h3("Y-axis"), 
                 choices = list("Sepal Length" = "Sepal.Length",
                                "Sepal Width" = "Sepal.Width"), 
                 selected = "Sepal.Length"),
     
-    sliderInput("slider", label = h3("Adjust markers"), min = 0, 
+      sliderInput("slider", label = h3("Adjust markers"), min = 0, 
                 max = 10, value = 3)
     ),
   
